c965effbb60ce1c98729e9518a4134717ae2223b
rayahakrosh <rraya879@gmail.com>
2025-11-22 09:28:10 +0200

    a new addition to the index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require('express');
-require('dotenv').config();
 const db = require('./config/db_config');
 const port = process.env.PORT;
 const api = process.env.HOST;
 const app = express();
-
 app.use(express.static(__dirname));
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/index.html')})
+app.use('/users',require('./routes/users_R'));
+app.use('/auth',require('./routes/auth_R'));
 
-app.use('/users', require('./routes/users_R'));
-
-app.listen(port, () => {
-    console.log(`http://${api}:${port}`);
-});
+app.listen(port,()=>{console.log(`http://${api}:${port}`)})
