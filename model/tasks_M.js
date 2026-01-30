0454c2bfd635edf663521885408b20ed07317d2e
rayahakrosh <rraya879@gmail.com>
2026-01-30 10:18:29 +0200

    add removeByCategoryId to tasks model for supporting cascade deletion

--- model/tasks_M.js
+++ model/tasks_M.js
@@ -1,42 +1,49 @@
 const db = require('../config/db_config');
 
-async function getAll(userId){
+async function getAll(userId) {
     let sql = `SELECT * FROM tasks WHERE user_id = ?`;
-    let [rows] = await db.query(sql,[userId]);    
+    let [rows] = await db.query(sql, [userId]);
     return rows;
 }
 
-async function add({text,userId,catId}){
+async function add({ text, userId, catId }) {
     let sql = `INSERT INTO tasks (text,user_id,category_id) VALUES (?,?,?)`;
-    let [result] = await db.query(sql,[text,userId,catId]); 
+    let [result] = await db.query(sql, [text, userId, catId]);
     return result.insertId;
 }
 
-async function getOne(taskId,userId){
+async function getOne(taskId, userId) {
     let sql = `SELECT * FROM tasks WHERE id = ? AND user_id = ?`;
-    let [result] = await db.query(sql,[taskId,userId]);    
+    let [result] = await db.query(sql, [taskId, userId]);
     return result[0];
 }
 
-async function remove(taskId,userId){
+async function remove(taskId, userId) {
     let sql = `DELETE FROM tasks WHERE id = ? AND user_id = ?`;
-    let [result] = await db.query(sql,[taskId,userId]);    
+    let [result] = await db.query(sql, [taskId, userId]);
     return result.affectedRows;
 }
 
-async function update(taskId,userId,newTask){
+async function removeByCategoryId(catId, userId) {
+    let sql = `DELETE FROM tasks WHERE category_id = ? AND user_id = ?`;
+    let [result] = await db.query(sql, [catId, userId]);
+    return result.affectedRows;
+}
+
+async function update(taskId, userId, newTask) {
     let keys = Object.keys(newTask);
     let values = Object.values(newTask);
-    let set = keys.map(k=>`${k}=?`).join(',');
+    let set = keys.map(k => `${k}=?`).join(',');
     let sql = `UPDATE tasks SET ${set} WHERE id = ? AND user_id = ?`;
-    let [result] = await db.query(sql,[...values,taskId,userId]);    
+    let [result] = await db.query(sql, [...values, taskId, userId]);
     return result.affectedRows;
 }
 
-module.exports ={
+module.exports = {
     getAll,
     add,
     getOne,
     remove,
+    removeByCategoryId, 
     update
-}
+};
