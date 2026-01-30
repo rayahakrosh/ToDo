9ac6e644f3fa5c45f4562ea148d4a98b49d14862
rayahakrosh <rraya879@gmail.com>
2026-01-30 10:55:13 +0200

    implement protected page routing and add management endpoints for users and categories

--- routes/pages_R.js
+++ routes/pages_R.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const { isLoggedIn } = require('../middelware/auth_MID'); 
 
-router.get('/',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","index.html"))})
-router.get('/reg',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","reg.html"))})
+router.get('/', isLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "pages", "index.html"));
+});
+
+router.get('/reg', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "pages", "reg.html"));
+});
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "pages", "login.html"));
+});
+
+router.get('/users_page', isLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "pages", "users_management.html"));
+});
+
+router.get('/cat_page', isLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "pages", "cat_management.html"));
+});
 
 module.exports = router;
